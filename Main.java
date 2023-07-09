e9dee326953924b066b57fd3acb660111fd22e61
Achimovicz <achimkrawczyk@outlook.de>
2023-07-09 19:58:15 +0200

    JavaprojektMET

---
+++ Main.java
@@ -0,0 +1,12 @@
+package REK;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Labyrinth lab = new Labyrinth('X', '*',30, 10);
+        System.out.println(lab);
+        // א
+    }
+}
